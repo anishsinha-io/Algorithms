517306a8c8648156a7c6ea3a37c673db9dbb49e9
Anish Sinha <anishsinha0128@gmail.com>
2021-07-14 01:51:43 -0400

    I have no idea why this works but it does

--- mergeSort.c
+++ mergeSort.c
@@ -5,39 +5,52 @@
 #include <string.h>
 
 void merge(int *, int, int, int);
-void print(int *, int);
+void mergeSort(int *, int, int);
+void print(int *);
 
 int main() {
-  int array[15] = {4, 11, 2, 6, 3, 14, 7, 10, 9, 2, 12, 15, 13, 1, 8};
-  int *arr = array;
-  int arraySize = sizeof(array) / sizeof(array[0]);
-  int p = 0, q = arraySize / 2, r = arraySize;
-  merge(arr, p, q, r);
-  for (int i = 0; i < r; i++) {
-    printf("%d ", array[i]);
+  int array[15] = {4, 11, 2, 6, 3, 14, 7, 10, 9, 5, 12, 15, 13, 1, 8};
+  mergeSort(array, 0, 15);
+  print(array);
+}
+
+void merge(int *A, int p, int q, int r) {
+  int n1 = q - p + 1;
+  int n2 = r - q;
+  int i, j, k;
+  int L[n1 + 1];
+  int R[n2 + 1];
+  for (i = 0; i < n1; i++) {
+    L[i] = A[p + i];
+  }
+  for (j = 0; j < n2; j++) {
+    R[j] = A[q + j + 1];
+  }
+  L[n1] = 1000;
+  R[n2] = 1000;
+  i = 0, j = 0;
+  for (k = p; k <= r; k++) {
+    if (L[i] <= R[j]) {
+      A[k] = L[i];
+      i++;
+    } else {
+      A[k] = R[j];
+      j++;
+    }
   }
 }
 
-void merge(int *arr, int p, int q, int r) {
-  int n1 = q - p + 1;
-  int n2 = r - q;
+void mergeSort(int *A, int p, int r) {
+  if (p < r) {
+    int q = (p + r) / 2;
+    mergeSort(A, p, q);
+    mergeSort(A, q + 1, r);
+    merge(A, p, q, r);
+  }
+}
 
-  int subArray1[q];
-  for (int i = p; i < n1; i++) {
-    subArray1[i] = arr[i];
-  }
-  int subArray2[q];
-  for (int j = 1; j < n2; j++) {
-    subArray2[j] = arr[q + j];
-  }
-  int i, j = 1;
-  for (int k = 0; k < r; k++) {
-    if (subArray1[i] < subArray2[i]) {
-      arr[k] = subArray1[i];
-      i++;
-    } else {
-      arr[k] = subArray2[j];
-      j++;
-    }
+void print(int *arr) {
+  for (int i = 0; i < 15; i++) {
+    printf("%d ", arr[i]);
   }
 }
